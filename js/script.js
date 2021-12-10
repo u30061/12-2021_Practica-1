a80475fe1381477e27980975b7cfab95131e8118
u30061 <92489650+u30061@users.noreply.github.com>
2021-12-10 05:35:25 +0000

    estado: funcional

--- js/script.js
+++ js/script.js
@@ -11,10 +11,18 @@ function registro()
     let nombre_completo = document.getElementById("nombre-completo").value;
     let email = document.getElementById("email").value;
     let password_registro = document.getElementById("password-registro").value;
+    let password_registro_repetir = document.getElementById("password-registro-repetir").value;
 
-    let cadena = "nombre-completo: "+nombre_completo + ", email: "+email + ", password: "+password_registro;
-    console.log(cadena);
-    alert(cadena);
+    if (password_registro===password_registro_repetir)
+    {
+        let cadena = "nombre-completo: " + nombre_completo + ", email: " + email + ", password: " + password_registro;
+        console.log(cadena);
+        alert(cadena);
+    }
+        else
+        {        
+            alert("CONTRASENIAS NO COINCIDEN")
+        }
 }
 
 function agregar() {
@@ -24,6 +32,8 @@ function agregar() {
     var apellidos = document.getElementById("apellidos").value;
     var cui = document.getElementById("cui").value;
     var departamento = document.getElementById("departamento").value;
-
-    listaIngresados.innerHTML = listaIngresados.innerHTML + "<li>" + cui + ", " + nombres + " " + apellidos + ", " + departamento + "</li>";
+    let cadena1 = cui + ", " + nombres + " " + apellidos + ", " + departamento
+    let cadena2 = "<li>" + cadena1 + "</li>"
+    console.log(cadena1);
+    listaIngresados.innerHTML = listaIngresados.innerHTML + cadena2;
 }
